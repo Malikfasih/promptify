8ef903b92fabe8c7b3534fa66ed8d926edb17c31
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2023-06-13 15:19:43 +0500

    removed unneccessary code

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,29 +1,14 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const GET = async (request, response) => {
+export const GET = async (request) => {
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
 
-    // response.headers.set("Cache-Control", "no-cache"); // Disable caching
-
     return new Response(JSON.stringify(prompts), { status: 200 });
-    // return {
-    //   status: 200,
-    //   headers: {
-    //     "Cache-Control": "no-cache", // Disable caching
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(prompts),
-    // };
   } catch (error) {
-    console.error("Failed to fetch all prompts:", error); // Log the error for debugging purposes
     return new Response("Failed to fetch all prompts", { status: 500 });
-    // return {
-    //   status: 500,
-    //   body: "Failed to fetch all prompts",
-    // };
   }
 };
