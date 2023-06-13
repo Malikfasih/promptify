f44f3d3e7d5b595f0255dbf02750d3fcd0a4ece2
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2023-06-13 16:46:37 +0500

    added new response

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,17 +1,13 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const GET = async ({ request, response }) => {
+export const GET = async (request) => {
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
-    response.setHeader(
-      "Cache-Control",
-      "public, s-maxage=10, stale-while-revalidate=59"
-    );
-    response.status(200).json(prompts);
-    // return new Response(JSON.stringify(prompts), { status: 200 });
+    // response.status(200).json(prompts);
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     console.log("this is an error", error);
     return new Response("Failed to fetch all prompts", { status: 500 });
