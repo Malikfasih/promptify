ee445a3ac912814f2a9754b253ca0af6c7c50665
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2023-06-13 16:27:08 +0500

    updated header

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,18 +1,16 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const GET = async (request, response) => {
+export const GET = async ({ request, response }) => {
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
-    response.setHeader(
-      "Cache-Control",
-      "public, s-maxage=10, stale-while-revalidate=59"
-    );
+    response.setHeader("Cache-Control", "no-cache");
     response.status(200).json(prompts);
     // return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
+    console.error("this is an error", error);
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
