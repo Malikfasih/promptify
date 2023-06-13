e241edc7bbddc9c67c8b2409baac13b95f9c79c3
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2023-06-13 15:36:01 +0500

    added headers in response

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,17 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const GET = async (request) => {
+export const GET = async (request, response) => {
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
-
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    response.setHeader(
+      "Cache-Control",
+      "public, s-maxage=10, stale-while-revalidate=59"
+    );
+    response.status(200).json(prompts);
+    // return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
