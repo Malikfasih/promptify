2d69e99f80d270586532272f1865914d63ed783b
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2023-06-13 11:18:29 +0500

    serverless function logic

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -7,10 +7,23 @@ export const GET = async (request, response) => {
 
     const prompts = await Prompt.find({}).populate("creator");
 
-    response.setHeader("Cache-Control", "no-cache"); // Disable caching
+    // response.headers.set("Cache-Control", "no-cache"); // Disable caching
 
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    // return new Response(JSON.stringify(prompts), { status: 200 });
+    return {
+      status: 200,
+      headers: {
+        "Cache-Control": "no-cache", // Disable caching
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(prompts),
+    };
   } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 500 });
+    console.error("Failed to fetch all prompts:", error); // Log the error for debugging purposes
+    // return new Response("Failed to fetch all prompts", { status: 500 });
+    return {
+      status: 500,
+      body: "Failed to fetch all prompts",
+    };
   }
 };
