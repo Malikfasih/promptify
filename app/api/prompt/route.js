8a8dfe2dc5d443e8048e1a8716902224b610b906
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2023-06-13 12:56:49 +0500

    refactor block of code

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -9,21 +9,21 @@ export const GET = async (request, response) => {
 
     // response.headers.set("Cache-Control", "no-cache"); // Disable caching
 
-    // return new Response(JSON.stringify(prompts), { status: 200 });
-    return {
-      status: 200,
-      headers: {
-        "Cache-Control": "no-cache", // Disable caching
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(prompts),
-    };
+    return new Response(JSON.stringify(prompts), { status: 200 });
+    // return {
+    //   status: 200,
+    //   headers: {
+    //     "Cache-Control": "no-cache", // Disable caching
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(prompts),
+    // };
   } catch (error) {
     console.error("Failed to fetch all prompts:", error); // Log the error for debugging purposes
-    // return new Response("Failed to fetch all prompts", { status: 500 });
-    return {
-      status: 500,
-      body: "Failed to fetch all prompts",
-    };
+    return new Response("Failed to fetch all prompts", { status: 500 });
+    // return {
+    //   status: 500,
+    //   body: "Failed to fetch all prompts",
+    // };
   }
 };
